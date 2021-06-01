d11dcb265eb6c5739360376601e3a9f9be0e983e
Allduiin <vovapro01@gmail.com>
2021-06-01 11:18:02 +0300

    CheckStyle

--- src/main/java/core/basesyntax/FootballMatch.java
+++ src/main/java/core/basesyntax/FootballMatch.java
@@ -16,10 +16,10 @@ public class FootballMatch {
      */
     public int matchResult(int result1, int result2, int bet1, int bet2) {
         int betResult;
-        betResult = (result1==bet1 && result2==bet2) ? 2:
-                (result1>result2 && bet1>bet2 ||
-                        result1<result2 && bet1<bet2 ||
-                        result1==result2 && bet1==bet2) ? 1:0;
+        betResult = (result1 == bet1 && result2 == bet2) ? 2 :
+                (result1 > result2 && bet1 > bet2
+                        || result1 < result2 && bet1 < bet2
+                        || result1 == result2 && bet1 == bet2) ? 1 : 0;
         return betResult;
     }
 }
